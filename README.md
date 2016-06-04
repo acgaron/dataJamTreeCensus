b0778950b5d6de784bf7e3625408719c9c3800fa
Andrew Garon <andygaron@gmail.com>
2016-06-04 11:38:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dataJamTreeCensus
+this is the repo for the datajam tree census
---
+++ tree_census_script.R
@@ -0,0 +1,12 @@
+#tree census and environmental / economic indicators
+
+#importing in dataset
+
+`2015_Street_Tree_Census_._Tree_Data` <- read.csv("~/Documents/DataJam/2015_Street_Tree_Census_-_Tree_Data.csv")
+
+treecensus <- `2015_Street_Tree_Census_._Tree_Data`
+
+tree_census_indicators <- read.csv("~/Documents/DataJam/tree_census_indicators.csv")
+
+treehumancensus <- merge(treecensus, tree_census_indicators, by.x = "cb_num", by.y = "cd")
+
